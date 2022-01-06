e9bbc8f31c0c4bdfa5eecbd206412b7d4c548f52
supunperera <7hashd@gmail.com>
2022-01-06 08:21:16 +0530

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
@@ -0,0 +1,16 @@
+package com.londonappbrewery.climapm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangeCityController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.change_city_layout);
+
+        
+    }
+}
